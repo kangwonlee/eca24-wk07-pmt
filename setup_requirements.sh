260bcc46be4759dd70617b125f38168b649838d3
KangWon LEE <kangwon@gmail.com>
2024-04-21 20:02:31 +0900

    use utility scripts in the workflow

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -1,1 +1,4 @@
-python -m pip install --upgrade pip && python -m pip install -r requirements.txt
+python -m pip install --upgrade pip
+if [ -f requirements.txt ]; then
+    pip install -r requirements.txt;
+fi
